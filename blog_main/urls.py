4baaf6051334c3c49f495b24bbb1e2269cfb1bf5
Abhinay25670 <224g1a3202@srit.ac.in>
2025-12-25 17:16:38 +0530

    Blog app and Blog Model

--- blog_main/urls.py
+++ blog_main/urls.py
@@ -1,6 +1,5 @@
 """
 URL configuration for blog_main project.
-
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/6.0/topics/http/urls/
 Examples:
@@ -17,7 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from .import views
+from django.conf.urls.static import static
+from django.conf import settings
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home,name='home'),
-]
+]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
